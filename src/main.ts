5ec23ba169f3b8228c0e549b5482c08ff07f1c1b
Leandro Sillos <sillos.adm@gmail.com>
2022-10-23 17:04:59 -0300

    add process.env.PORT in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { configuration } from './config/configuration'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
